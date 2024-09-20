685ec049dbded2f913763452009d07afccb720f1
Leo-Santos17 <leonardosm@protonmail.com>
2024-09-20 10:18:30 -0300

    test Windows

--- sketch_sep20a.ino
+++ sketch_sep20a.ino
@@ -1,23 +1,27 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
 // Defina o pino onde o LED está conectado
 const int ledPin = 14;  // GPIO5 (D1 no NodeMCU)
-
+float temp=0;
+int analog= 0;
 void setup() {
   // Configura o pino do LED como saída
   pinMode(ledPin, OUTPUT);
-  
-  // Inicializa o LED apagado
-  digitalWrite(ledPin, LOW);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
 }
 
 void loop() {
-  // Acende o LED
-  digitalWrite(ledPin, HIGH);
+  // Analog
+  analog= analogRead(17);
+  temp = analog*0.48828125;
   
-  // Aguarda 1 segundo (1000 milissegundos)
-  delay(1000);
-  
-  // Apaga o LED
-  digitalWrite(ledPin, LOW);
+  Serial.print("Temp: ");
+  Serial.print(temp);
   
   // Aguarda 1 segundo
   delay(1000);
